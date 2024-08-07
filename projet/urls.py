badb780eca47a6d67f0392218aba88142a94782a
monsieurlamah <monsieurlamah2024@gmail.com>
2024-08-07 09:18:48 +0000

    url commit

--- projet/urls.py
+++ projet/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('aguipex-admin/', admin.site.urls),
@@ -9,5 +11,8 @@ urlpatterns = [
     path('personnel/', include("userauths.urls")),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += staticfiles_urlpatterns()
